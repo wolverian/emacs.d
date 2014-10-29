ab10d7d7350361dbc1924e0f6c0fbbf07bac86bd
Ilmari Vacklin <ilmari.vacklin@reaktor.fi>
2014-10-29 12:35:54 +0200

    Add Pallet & Haskell

--- init.el
+++ init.el
@@ -1,42 +1,48 @@
 (require 'cask)
 (cask-initialize)
+(require 'pallet)
+
 (require 'use-package)
 
-;; settings
+;; visual settings
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message "iv")
 (load-theme 'minimal-light t)
 (set-default-font "Input Sans 13")
 
 ;; packages
 
 (use-package nyan-mode
-  :config (progn
-	    (nyan-mode)
-	    (nyan-start-animation)	   
-	    (setq nyan-wavy-trail t)))
+  :init (progn
+	  (nyan-mode)
+	  (nyan-start-animation))
+  :config (setq nyan-wavy-trail t))
 
 (use-package aggressive-indent
-  :config (global-aggressive-indent-mode))
+  :init (global-aggressive-indent-mode))
 
 (use-package helm
-  :config (progn
-            (use-package helm-config)
-	    (use-package helm-eshell)
-	    (use-package helm-files)
-	    (use-package helm-grep))
+  :init (progn
+	  (use-package helm-config)
+	  (use-package helm-eshell)
+	  (use-package helm-grep))
   :bind (("M-x" . helm-M-x)
-	 ("C-x b" . helm-mini)))
+	 ("C-x b" . helm-mini)
+	 ("C-x C-f" . helm-find-files)))
 
 (use-package smartparens
+  :init (use-package smartparens-config)
   :config (progn
-	    (use-package smartparens-config)
 	    (smartparens-global-mode)
 	    (show-smartparens-global-mode)))
 
 (use-package magit
   :bind (("C-, s" . magit-status)))
+
+(use-package haskell-mode)
 
 ;; non-mode specific keys
 
