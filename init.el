8a8038101f16ddba0038eefb23e0731c9b476d9f
Ilmari Vacklin <ilmari.vacklin@reaktor.fi>
2014-10-29 12:12:05 +0200

    First packages

--- init.el
+++ init.el
@@ -1,2 +1,21 @@
 (require 'cask)
 (cask-initialize)
+(require 'use-package)
+
+;; settings
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(load-theme 'minimal-light t)
+(set-default-font "Input Sans 13")
+
+;; packages
+
+(use-package nyan-mode
+  :config (progn
+	    (nyan-mode)
+	    (nyan-start-animation)	   
+	    (setq nyan-wavy-trail t)))
+
+(use-package aggressive-indent
+  :config (global-aggressive-indent-mode))
